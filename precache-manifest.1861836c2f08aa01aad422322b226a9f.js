74eb0efd9ddb93b54a2efccb4464df253e3cc661
umesh476 <umeshprajapati476@gmail.com>
2020-09-30 07:38:59 +0530

    Updates

---
+++ precache-manifest.1861836c2f08aa01aad422322b226a9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb476be272ede72434d14225685d05c",
+    "url": "/Uresume/index.html"
+  },
+  {
+    "revision": "cf5c50102581c3835bcb",
+    "url": "/Uresume/static/css/2.8ce86be7.chunk.css"
+  },
+  {
+    "revision": "6d5490be622b02b189f3",
+    "url": "/Uresume/static/css/main.297b6bcb.chunk.css"
+  },
+  {
+    "revision": "cf5c50102581c3835bcb",
+    "url": "/Uresume/static/js/2.3e48b604.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Uresume/static/js/2.3e48b604.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5490be622b02b189f3",
+    "url": "/Uresume/static/js/main.a2e4cbb5.chunk.js"
+  },
+  {
+    "revision": "e8d25ef5be0d1718f4be",
+    "url": "/Uresume/static/js/runtime-main.de313982.js"
+  },
+  {
+    "revision": "7f23972d50b63ed948a0e316280ab65d",
+    "url": "/Uresume/static/media/Resume_2.7f23972d.jpg"
+  }
+]);
